b7e84651a9874c4d197a3339a4807264acb1ae77
kyorohiro <kyorohiro@gmail.com>
2020-08-03 05:18:26 +0900

    ..

--- bin/isolate_ex.dart
+++ bin/isolate_ex.dart
@@ -1,5 +1,17 @@
 import 'dart:isolate' as iso;
 
 Future main() async {
-  
+  iso.ReceivePort receivePort = iso.ReceivePort();
+  iso.Isolate.spawn(workerMain, receivePort.sendPort);
+
+  receivePort.listen((message) {
+    print("host: ${message}");
+  });
+}
+
+workerMain(sendPort) async { 
+    for(var message in ["01","02","03"]) {
+        (sendPort as iso.SendPort).send(message);
+        await Future.delayed(Duration(seconds: 1));
+    }
 }
